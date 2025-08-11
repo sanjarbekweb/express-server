d3356e8dceb4b49e42c369ccd21c30ea94d9b736
sanjarbekweb <sanjarbeks650@gmail.com>
2025-08-11 17:31:35 +0500

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("salom");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/health', (req, res) => {
+    res.send({ message: "OK" });
+});
+
+app.listen(port, () => {
+    console.info("server started on port " + port);
+});
