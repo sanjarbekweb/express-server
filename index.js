04e5fb84fc0afd76f4e66ea264d3ba386a9b4bb3
arabjanov <arabjanovxojiakbar@gmail.com>
2025-08-11 17:45:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get('/health', (req, res) => {
     res.send({ message: "OK" });
 });
 
-app.listen(port, () => {
-    console.info("server started on port " + port);
+app.listen(PORT, () => {
+    console.info("server started on port " + PORT);
 });
